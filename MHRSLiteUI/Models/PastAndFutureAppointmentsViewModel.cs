8b9498aa54ca16ae730dacd13cc2004b8002ad37
birkanvaranofficial@gmail.com <birkanvaranofficial@gmail.com>
2022-02-28 14:24:41 +0300

    ViewImports ekleme yapıldı. PatientController'da SaveAppointment'ta email gönderdik. PatientController'a Randevu iptali eklendi. PatientController'a Excel Export eklendi.

--- MHRSLiteUI/Models/PastAndFutureAppointmentsViewModel.cs
+++ MHRSLiteUI/Models/PastAndFutureAppointmentsViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using MHRSLiteEntityLayer.Model;
+using MHRSLiteEntityLayer.PagingListModels;
+using MHRSLiteEntityLayer.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +10,11 @@ namespace MHRSLiteUI.Models
 {
     public class PastAndFutureAppointmentsViewModel
     {
-        public List<Appointment> PastAppointments { get; set; }
-        public List<Appointment> UpcomingAppointments { get; set; }
+        //public List<Appointment> PastAppointments { get; set; }
+        //public List<Appointment> UpcomingAppointments { get; set; }
+
+        // Randevuları sayfalama yaparak görüntülemek istediğim için PaginatedList kullandık.
+        public PaginatedList<AppointmentVM> PastAppointments { get; set; }
+        public PaginatedList<AppointmentVM> UpcomingAppointments { get; set; }
     }
 }
